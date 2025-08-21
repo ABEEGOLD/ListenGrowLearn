5cd4d232734cd28049e7131aba18c911c3fcec02
ABEEGOLD <abeeutee@gmail.com>
2025-08-21 13:57:52 +0100

    task

---
+++ TwoListenGrowLearn/TaskFive.java
@@ -0,0 +1,15 @@
+public class TaskFive{
+	public static void main(String[] args){
+	
+	for(int count = 50 + 1; count <= 100;count +=2){
+		System.out.println(count);
+		
+	}
+
+
+
+	}
+
+
+
+}
---
+++ TwoListenGrowLearn/TaskSix.java
@@ -0,0 +1,16 @@
+public class TaskSix{
+	public static void main(String[] args){
+	
+	for(int count = 50; count <= 100;count ++){
+		if(count % 2 != 0){
+			System.out.println(count);
+		}
+	}
+
+
+
+	}
+
+
+
+}
