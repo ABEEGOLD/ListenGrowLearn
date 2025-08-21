8760466f8d885eda0da745957313b8021ee7e13c
ABEEGOLD <abeeutee@gmail.com>
2025-08-21 17:38:42 +0100

    tasks

---
+++ TwoListenGrowLearn/TaskThirteen.java
@@ -0,0 +1,20 @@
+public class TaskThirteen{
+	public static void main(String[] args){
+	
+		int sum = 0;
+	for(int count = 1; count <= 50 ;count ++){
+			sum +=count;	
+			System.out.println("The sum of the first natural number is:"+sum);
+
+
+		
+		
+	}
+
+
+
+	}
+
+
+
+}
