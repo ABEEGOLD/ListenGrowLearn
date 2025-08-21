b0dfb94a049736e9fa02b71951c37b211b8bd0ae
ABEEGOLD <abeeutee@gmail.com>
2025-08-21 06:12:25 +0100

    update

--- LearnGrowListen/TablesSquareCube.java
+++ LearnGrowListen/TablesSquareCube.java
@@ -1,33 +1,44 @@
 public class TablesSquareCube{
 	public static void main(String[] args){
 
-	int number = 0 * 0;
-	int square = number * number;
-	int cube = number * number * number;
+	
+	int number = 0;
+	int square = number * 2;
+	int cube = number * 3;
 
 	System.out.println("\tNumber\tSquare\tCube");
 
-	System.out.println("=========================");
+	System.out.println("============================");
 
 	                                            
-	System.out.printf(number+ "\t" +square+ "\t" +cube); 
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 1;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 2;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 3;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 4;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 5;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 6;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 7;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 8;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 9;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+	number = 10;
+	System.out.printf("\t%d\t%d\t%d\n",number,number * number,number * number * number); 
+
+
+
+
 
 	                                                                                                                              
-			System.out.println(0);
-			System.out.println(1);
-			System.out.println(2);
-			System.out.println(3);
-			System.out.println(4);
-			System.out.println(5);
-			System.out.println(6);
-			System.out.println(7);
-			System.out.println(8);
-			System.out.println(9);
-			System.out.println(10);
-
-
-
-
+			
 	}
 
 
