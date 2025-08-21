3ce575c2e4e433703ffda7a01dde261f6f10771a
ABEEGOLD <abeeutee@gmail.com>
2025-08-21 01:48:18 +0100

    update

---
+++ LearnGrowListen/TablesSquareCube.java
@@ -0,0 +1,34 @@
+public class TablesSquareCube{
+	public static void main(String[] args){
+
+	int number = 0 * 0;
+	int square = number * number;
+	int cube = number * number * number;
+
+	System.out.println("\tNumber\tSquare\tCube");
+
+	System.out.println("=========================");
+
+	                                            
+	System.out.printf(number+ "\t" +square+ "\t" +cube); 
+
+	                                                                                                                              
+			System.out.println(0);
+			System.out.println(1);
+			System.out.println(2);
+			System.out.println(3);
+			System.out.println(4);
+			System.out.println(5);
+			System.out.println(6);
+			System.out.println(7);
+			System.out.println(8);
+			System.out.println(9);
+			System.out.println(10);
+
+
+
+
+	}
+
+
+}
