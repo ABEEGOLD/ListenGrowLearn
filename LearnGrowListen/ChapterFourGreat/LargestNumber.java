624675aff02410749304dda5e2154fb1e3244bc9
ABEEGOLD <abeeutee@gmail.com>
2025-09-07 00:00:20 +0100

    task

--- LearnGrowListen/ChapterFourGreat/LargestNumber.java
+++ LearnGrowListen/ChapterFourGreat/LargestNumber.java
@@ -3,22 +3,31 @@ import java.util.Scanner;
 public class LargestNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-	System.out.print("Enter a number: ");
-	int number = input.nextInt();
-
+	//System.out.print("Enter your name: ");
+	//String name = input.nextLine();
+			
 		int largest = 0;
-
- 	for(int count = 1;count <= 10;count++){
-		if(count == 10){
-		    System.out.println(count);
-		}
-
-	}
-
+		//int smallest = 0;
+		int counter = 1;
+ 	while(counter <= 9){
+	counter++;
+	System.out.print("Enter a number: ");
+		int number = input.nextInt();
+		
 	if(number > largest){
 		largest = number;
 	}
+	
+	/*if(number < smallest){
+		smallest = number;
+	}*/
+	}
+	//System.out.println("Miss:" +name);
+	System.out.println("Count is:" +counter);
+	System.out.println("The largest number is:"+largest);
+	//System.out.println("The smallest number is:"+smallest);
 
+   
 
    }
 }
---
+++ LearnGrowListen/ChapterFourGreat/TableOutput.java
@@ -0,0 +1,28 @@
+public class TableOutput {
+    public static void main(String[] args){
+
+
+	System.out.println("N" "\tN2" "\tN3\" "\tN4\");
+	
+
+	for(int count = 1;count <= 4;count *=2){
+		count++;
+
+	} 
+	 System.out.println("\t"count = count * 1);
+	 System.out.println("\t"count = count * 2);
+	 System.out.println("\t"count = count * 3);
+	 System.out.println("\t"count = count * 4);
+	 System.out.println("\t"count = count * 5);
+
+
+
+
+
+
+
+
+
+ 
+   }
+}
